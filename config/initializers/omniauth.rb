cc80e318671b3a423e8d763034609bf135b5f55b
SarCasm <sarcasm008@gmail.com>
2018-02-05 23:37:32 +0200

    Heroku fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require "omniauth/strategies/steam"
 require "openid/store/filesystem"
 
 puts "Please inform your Steam API key: "
-api_key = ENV["STEAM_WEB_API_KEY"] || STDIN.gets.strip
+api_key = ENV["STEAM_WEB_API_KEY"]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :steam, api_key, :storage => OpenID::Store::Filesystem.new("/tmp")
